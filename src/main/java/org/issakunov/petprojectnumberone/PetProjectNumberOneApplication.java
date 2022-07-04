98898bb176bced3bef64d09f5c65fce61d4fec31
atabek <ataissakunov@gmail.com>
2022-07-04 10:39:39 +0600

    initial project commit

---
+++ src/main/java/org/issakunov/petprojectnumberone/PetProjectNumberOneApplication.java
@@ -0,0 +1,13 @@
+package org.issakunov.petprojectnumberone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectNumberOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectNumberOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/issakunov/petprojectnumberone/PetProjectNumberOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.issakunov.petprojectnumberone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectNumberOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
